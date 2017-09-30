0d26a4dfcc20ae86ec00d5b7fee77884d0a02489
Rod Nolan <rodnolan@gmail.com>
2017-09-30 14:16:07 -0400

    intalled, linked and configured react-native-facebook-account-kit

--- android/app/src/main/java/com/posterific/MainApplication.java
+++ android/app/src/main/java/com/posterific/MainApplication.java
@@ -3,6 +3,7 @@ package com.posterific;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.underscope.react.fbak.RNAccountKitPackage;
 import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAccountKitPackage(),
             new RNViewShotPackage(),
             new PickerPackage()
       );
